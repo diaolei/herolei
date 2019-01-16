a95246e01b8d0038a896d8d9d98036aeff5cda13
herolei <276317545@qq.com>
2019-01-16 10:59:22 +0800

    第二次提交

--- app/src/main/java/seuic/com/flashlight/MainActivity.java
+++ app/src/main/java/seuic/com/flashlight/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
 
 
+		//添加多一句注解
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         }
